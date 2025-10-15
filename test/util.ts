ca7fce19e8bf4e77932da06a25f7ad527fe7a468
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2025-10-15 14:20:21 +0200

    increase test timeout

--- test/util.ts
+++ test/util.ts
@@ -179,7 +179,7 @@ export function setup({
       const httpServer = createServer();
       const io = new Server(httpServer, {
         adapter: createAdapter(redisClient, {
-          readCount: 1, // return as soon as possible
+          blockTimeInMs: 20, // reduce the polling interval to speed up the tests
         }),
         ...serverOptions,
       });
